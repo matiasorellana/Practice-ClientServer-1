f621581f97a59f9c00957770135aac6e4e5743e1
matiasorellana <matias.orellana.campos@gmail.com>
2024-06-27 21:08:33 -0600

    Navegation completed

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+const PUBLIC = path.join(__dirname, 'public');
+
+app.use(express.urlencoded({extended: true }));
+app.use(express.static(PUBLIC));
+
+app.get('/', (req, res)=>{
+    console.log('Loading home...');
+    res.sendFile(path.join(PUBLIC,'home.html'));
+});
+
+app.get('/contactus',(req, res) => {
+console.log('Loading contact us...');
+res.sendFile(path.join(PUBLIC, 'contactus.html'));
+});
+
+app.post('/contactus', (req, res) => {
+    console.info('Contac Us has been called...\n');
+    const name = req.body.name;
+    const email = req.body.email;
+    const subject = req.body.subject;
+
+    console.log('Form data\n');
+    console.log('Name: ' + name);
+    console.log('Email: ' + email);
+    console.log('Subject: ' + subject);
+
+    res.redirect('/');
+});
+
+app.listen(PORT, () => {
+    console.log('🥑 Server is running on port' + PORT);
+});
+
+
+
